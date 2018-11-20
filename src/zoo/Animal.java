b2d7dc85d49d38f2fc27601d2ff700b6d41d105f
Mélanie Pichery <melanie.pichery@laposte.net>
2018-11-20 12:35:40 +0100

    Ajout package zoo classe Animal

---
+++ src/zoo/Animal.java
@@ -0,0 +1,37 @@
+package zoo;
+
+public class Animal {
+
+	private String nom;
+	private String type;
+	
+	
+	public Animal(String nom, String type) {
+
+		this.nom = nom;
+		this.type = type;
+		
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+		
+	
+}
